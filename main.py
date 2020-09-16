fb8389c758a35a349abf5aa8ef46b8a81ea1bdc2
Isaac N. Dominguez <Isaacdominguez41@gmail.com>
2020-09-16 05:01:03 -0700

    Test commit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 
 
+# This is my first project using python and Pygame. Hoping to create various simple games.
+# Including but not limited to: Snake Game, Space Invaders, Pong, Super Mario, and Tron.
+
 def __init__(self):
     print("idk")
 

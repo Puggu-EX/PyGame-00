3afde0682a6cd0e2c4162d45d320e189b05a1a2f
Isaac N. Dominguez <Isaacdominguez41@gmail.com>
2020-09-20 14:46:19 -0700

    Base Flappy Bird Game. BG and Moving Floor

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-import pygame, sys
+import pygame
+import sys
+import random
+import time
 
 
 # This is my first project using python and Pygame. Hoping to create various simple games.
 # Including but not limited to: Snake Game, Space Invaders, Pong, Super Mario, and Tron.
 
 def create_pipe():
-    new_pipe = pipe_surface.get_rect(midtop=(screen_w // 4, screen_h // 4))
-    return new_pipe
+    # random_pipe_pos = random.choice(pipe_height)
+    # new_pipe_top = pipe_surface.get_rect(midbottom=((screen_w // 2) + 50, random_pipe_pos - 140))
+    # new_pipe = pipe_surface.get_rect(midtop=((screen_w // 2) + 50, random_pipe_pos))
+
+    x = random.randint(200,350)
+    new_pipe_top = pipe_surface.get_rect(midbottom=((screen_w // 2) + 50, x - 140))
+    new_pipe = pipe_surface.get_rect(midtop=((screen_w // 2) + 50, x))
+    return new_pipe, new_pipe_top
 
 
 def move_pipes(pipes):
@@ -17,7 +26,21 @@ def move_pipes(pipes):
 
 def draw_pipes(pipes):
     for pipe in pipes:
-        screen.blit(pipe_surface, pipe)
+        if pipe.bottom >= 1024 // 2:
+            screen.blit(pipe_surface, pipe)
+        else:
+            screen.blit(flip_pipe, pipe)
+
+
+def check_collision(pipes):
+    for pipe in pipes:
+        if bird_rect.colliderect(pipe):
+            return False
+    if bird_rect.top <= -50:
+        return False
+    elif bird_rect.bottom >= 400:
+        return False
+    return True
 
 
 def draw_floor():
@@ -32,55 +55,77 @@ pygame.init()
 screen = pygame.display.set_mode((screen_w // 2, screen_h // 2))
 clock = pygame.time.Clock()
 
-# Game Variables
-gravity = .25
+###### Game Variables #######
+gravity = .15
 bird_movement = 0.0
 
+game_active = True
+
+# Background Surface
 bg_surface = pygame.image.load('assets/background-day.png').convert()
 # bg_surface = pygame.transform.scale2x(bg_surface)
 
+# Floor Surface
 floor_surface = pygame.image.load('assets/base.png').convert()
 floor_surface = pygame.transform.scale(floor_surface, (576, 115))
 floor_x_pos = 0
 
+# Bird Surface
 bird_surface = pygame.image.load('assets/bluebird-midflap.png').convert()
 # bird_surface = pygame.transform.scale2x(bird_surface)
 bird_rect = bird_surface.get_rect(center=((screen_w // 4) - 100, screen_h // 4))
 
+# Pipe Surface, List, Entity, Spawn Timer, Height List
 pipe_surface = pygame.image.load('assets/pipe-green.png').convert()
+flip_pipe = pygame.transform.flip(pipe_surface, False, True)
 pipe_list = []
 SPAWNPIPE = pygame.USEREVENT
 pygame.time.set_timer(SPAWNPIPE, 1200)
+pipe_height = [200, 250, 300, 350]
 
 while True:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_SPACE and game_active:
                 bird_movement = 0.0
-                bird_movement -= 6.0
+                bird_movement -= 4.0
+            elif event.key == pygame.K_SPACE and game_active == False:
+                game_active = True
+                pipe_list.clear()
+                bird_rect.center = (100, 250)
+                bird_movement = 0
             elif event.key == pygame.K_BACKSPACE:
                 pygame.quit()
                 sys.exit()
         if event.type == SPAWNPIPE:
-            print("Pipe Spawn")
-            pipe_list.append(create_pipe())
+            pipe_list.extend(create_pipe())
+            # print("Pipe Spawn: ", len(pipe_list))
 
-    # Bird Movement
-    bird_movement += gravity
-    bird_rect.centery += bird_movement
+            # De-spawn Pipes
+            if len(pipe_list) > 6:
+                pipe_list.pop(0)
+                pipe_list.pop(1)
 
     # Draws surfaces
     screen.blit(bg_surface, (0, 0))
-    screen.blit(bird_surface, bird_rect)
 
-    # Pipes
-    pipe_list = move_pipes(pipe_list)
-    draw_pipes(pipe_list)
+    if game_active:
+        # Pipes
+        pipe_list = move_pipes(pipe_list)
+        draw_pipes(pipe_list)
 
+        # Bird Movement
+        bird_movement += gravity
+        bird_rect.centery += bird_movement
+        screen.blit(bird_surface, bird_rect)
+
+        # Keep Game Active / Check Collision
+        game_active = check_collision(pipe_list)
+
+    # Floor
     draw_floor()
     floor_x_pos -= 1
     if floor_x_pos == -576 // 2:

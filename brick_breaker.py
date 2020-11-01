a646f32b1571e7f1e33ab8e3fbe416857a909033
Isaac N. Dominguez <Isaacdominguez41@gmail.com>
2020-10-31 18:52:52 -0700

    Pairing a rectangle with a surface based off the rectangle's position in the array caused the issue that when a rectangle was popped off further in the array the colors would shift. The first work around was to just replace the popped off index with a new rectangle that couldn't be seen. This works until I consider adding power-ups in the place of a destroyed brick. (it would just be given the surface the popped off rectangle would have been given)
    The new method pairs rectangles in one list (brick_list) to a surface in another list (brick_surface_list). When a brick is destroyed there is a random chance that the rectangle and the surface are discarded or replaced with a new rectangle and surface (power up).

--- brick_breaker.py
+++ brick_breaker.py
@@ -1,12 +1,12 @@
 import sys
-import pygame
 from random import randint
+
+import pygame
 
 
 def draw_bricks(brick, surface):
     for bricks, surfaces in zip(brick, surface):
         screen.blit(surfaces, bricks)
-
 
 def delete_brick():
     pass
@@ -46,12 +46,27 @@ brick_surface_cyan_02 = pygame.image.load('assets/brick_breaker_assets/12-Breako
 brick_surface_cyan = brick_surface_cyan_01
 brick_surface_cyan = pygame.transform.scale(brick_surface_cyan, (85, 35))
 
+# Background Surface
 background_surface = pygame.image.load('assets/background-night.png')
 background_surface = pygame.transform.scale(background_surface, (1280, 720))
+
+# Ball Surface & Rectangle
+ball_x = 600
+ball_y = 600
+ball_surface = pygame.image.load('assets/brick_breaker_assets/ball.png')
+ball_surface = pygame.transform.scale(ball_surface, (40, 40))
+ball_rect = pygame.draw.circle(ball_surface, 0, (ball_x, ball_y), 0)
+
+# Paddle Surface & Rectangle
+paddle_x = 640
+paddle_y = 680
+paddle_surface = pygame.image.load('assets/brick_breaker_assets/paddle.png')
+paddle_surface = pygame.transform.scale(paddle_surface, (200, 40))
 
 CREATEBRICK = pygame.USEREVENT
 pygame.time.set_timer(CREATEBRICK, 1000)
 
+# Create Brick Rectangle List
 brick_list = []
 brick_list.clear()
 count_x = 45
@@ -66,6 +81,7 @@ for i in range(5):
             count_x = 45
         brick_list.append(new_brick)
 
+# Create Brick Surface List
 brick_surface_list = []
 brick_surface_list.clear()
 count = 0
@@ -86,7 +102,8 @@ for i in range(60):
 """
 Game Loop
 """
-
+k_left_state = 0
+k_right_state = 0
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -96,14 +113,35 @@ while True:
             pass
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                brick_list.pop(0)
-                brick_surface_list.pop(0)
+                x = randint(0, 10)
+                brick_list.pop(x)
+                brick_surface_list.pop(x)
+            if event.key == pygame.K_LEFT:
+                k_left_state += -5
+            if event.key == pygame.K_RIGHT:
+                k_right_state += 5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                k_left_state = 0
+            if event.key == pygame.K_RIGHT:
+                k_right_state = 0
 
     # Draw Background
     screen.blit(background_surface, (0, 0))
 
     # Draw Bricks
     draw_bricks(brick_list, brick_surface_list)
+
+    # Draw Ball
+    ball_rect = pygame.draw.circle(ball_surface, 0, (ball_x+20, ball_y), 0)
+    screen.blit(ball_surface, ball_rect)
+
+    # Draw Paddle
+    paddle_x += k_left_state
+    paddle_x += k_right_state
+    paddle_mask = pygame.mask.from_surface(paddle_surface)
+    screen.blit(paddle_surface,paddle_rect)
+    print(paddle_x)
 
     pygame.display.update()
     clock.tick(144)
